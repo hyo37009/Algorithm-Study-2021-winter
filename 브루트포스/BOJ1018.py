7b551061fc89bace743c8fc938b1f75351547495
JIYE KIM <hyo37009@gmail.com>
2021-12-31 16:49:25 +0900

    ## BOJ1018 체스판 다시 칠하기 ## # 적당히 칠해진 판을 8*8로 잘라 체스판처럼 지그제그로 다시 칠하는데, # 체스판 모양으로 만들기 위해 가장 최소로 칠하는 수를 찾는 문제
    검사를 위한 8*8 체스판을 다시 만드는 데에서 많이 헤맸다... 기본적인 거니까 다음부터는 실수가 줄을 거라 생각한다.

--- 브루트포스/BOJ1018.py
+++ 브루트포스/BOJ1018.py
@@ -1,30 +1,44 @@
 ## BOJ1018 체스판 다시 칠하기 ##
 # 적당히 칠해진 판을 8*8로 잘라 체스판처럼 지그제그로 다시 칠하는데,
 # 체스판 모양으로 만들기 위해 가장 최소로 칠하는 수를 찾는 문제
+def chess(now=[])->int:
+    stWhi = 0  # 화이트로 시작하는 체스판 검사
+    stBla = 64  # 블랙으로 시작하는 체스판 검사
+    pre = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]  # 체스판 설정(흰색 시작)
+
+    # 검사 시작
+    for a in range(8):
+        for b in range(8):
+            if now[a][b] != pre[a][b]:
+                stWhi += 1
+                stBla -= 1
+
+    return stWhi if stWhi < stBla else stBla
+
 
 n, m = map(int, input().split(" "))
 board = []
-chess = [0, 0, 0, 0, 0, 0, 0, 0]
-now = 0
-less = 32  # 최댓값
+less = 32
+nowlist = []
 
-for _ in range(n):
+for k in range(n):
     board.append(input())
+    board[k] = list(board[k])
 
-for i in range(n - 7): #줄검사
-    for j in range(m - 7):
-        for tem in range(8):
-            chess[tem] = list(board[tem][j:j+8])
-        for a in range(8):
-            for b in range(7):
-                if a != 0 & b == 0:
-                    if chess[a - 1][0] == chess[a][0]:
-                        now += 1
-                        chess[a][0] = 'B' if chess[a - 1][0] != 'B' else 'W'
-                if chess[a][b] == chess[a][b + 1]:
-                    now += 1
-                    chess[a][b + 1] = 'B' if chess[a][b + 1] != 'B' else 'W'
-        if now < less: less = now
-        now = 0
+for i in range(n-7):
+    for j in range(m-7):
+        for k in range(8):
+            nowlist.append(board[i+k][j:j+9])
+        nowless = chess(nowlist)
+        if less > nowless:
+            less = nowless
+        nowlist = []
 
 print(less)
