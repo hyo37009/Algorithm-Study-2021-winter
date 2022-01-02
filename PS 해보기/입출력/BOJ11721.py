199184d8098c47c0387ea830debed247e0bd1ddf
JIYE KIM <hyo37009@gmail.com>
2022-01-02 22:07:11 +0900

    ## 그대로 출력하기 2 ##
    and로 합쳤다. 아까 &으로 합칠 때 a != 0 이런식으로 잘못쓴듯

--- PS 해보기/입출력/BOJ11721.py
+++ PS 해보기/입출력/BOJ11721.py
@@ -5,7 +5,5 @@ n = 0
 
 for i in range(len(a)):
     print(a[i], end='')
-    if (i + 1) % 10 == 0 : #왠지 and로 합치면 동작을 안한다...
-        if i == 0:
-            continue
+    if (i + 1) % 10 == 0 and i != 0:
         print()
