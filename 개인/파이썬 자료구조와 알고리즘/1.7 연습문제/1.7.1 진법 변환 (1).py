f3db48dcc5697dc0ba00804b62e1526dd6fcff41
JIYE KIM <hyo37009@gmail.com>
2022-01-03 21:58:49 +0900

    내가 가지고 있는 고등학교 수학 지식으로 구현한 코드이다. reverse의 사용법이 헷갈린다. [::-1]이 편하다.

---
+++ 개인/파이썬 자료구조와 알고리즘/1.7 연습문제/1.7.1 진법 변환 (1).py
@@ -0,0 +1,42 @@
+'''
+내가 가지고 있는 고등학교 수학 지식으로 구현한 코드이다.
+reverse의 사용법이 헷갈린다. [::-1]이 편하다.
+'''
+
+def two(num):
+    list = []
+    while num >= 2:
+        list.append(str(num%2))
+        num //= 2
+    list.append(str(num))
+
+    return (''.join(list))[::-1]
+
+def eight(num):
+    list = []
+
+    while num >= 8:
+        list.append(str(num%8))
+        num //= 8
+    list.append(str(num))
+
+    return (''.join(list))[::-1]
+
+def sth(num):
+    numlist = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+    list = []
+
+    while num >= 16:
+        list.append(str(numlist[num%16]))
+        num //= 16
+    list.append(str(numlist[num%16]))
+
+    return (''.join(list))[::-1]
+
+n = 846
+print('0b' + two(n))
+print(bin(n))
+print('0o' + eight(n))
+print(oct(n))
+print('0x' + sth(n))
+print(hex(n))
